15607f5fa4e80ae339d7fe121cb324760da37a7f
apricityx <apricityx@qq.com>
2024-09-28 01:31:20 +0800

    Dev. Finished. Todo. 作业完成情况API

--- src/api/v1/am/get_am.ts
+++ src/api/v1/am/get_am.ts
@@ -1,6 +1,47 @@
-import {db} from "@/utils/loadDB";
+import {db, dbAM} from "@/utils/loadDB";
+import {Request, Response} from "express";
+import {logger} from "@/utils/logger";
 
-export const get_am = (req: any, res: any) => {
-    const AMList = db.prepare('SELECT name, deadline FROM AM').all()
-    res.send(AMList)
+interface AssignmentPreview {
+    name: string
+    deadline: string
+    show?: boolean
+    description: string
+    total: number
+    submitted: number
+}
+
+interface AssignmentDetailed {
+    name: string
+    deadline: string
+    show?: boolean
+    description: string
+}
+
+export const get_am = (req: Request, res: Response) => {
+    // 应当设置requestType ?request_type=preview | detailed
+    const requestType = req.query.request_type as "preview" | "detailed"
+    if (requestType === "preview") {
+        const AMList = db.prepare('SELECT name, deadline, description FROM AM').all() as AssignmentPreview[]
+        for (const i in AMList) {
+            const submission = dbAM.prepare(`SELECT COUNT(CASE WHEN is_submitted = 1 THEN 1 END) AS submitted,COUNT(*) AS total FROM ${AMList[i].name}`).get() as {
+                submitted: number
+                total: number
+            }
+            logger.debug(submission)
+            AMList[i].total = submission.total
+            AMList[i].submitted = submission.submitted
+        }
+        res.send(AMList)
+        return
+    } else if (requestType === "detailed") {
+        const AMList = db.prepare('SELECT name, deadline, description FROM AM').all() as AssignmentPreview[]
+        res.send(AMList)
+        return;
+    } else {
+        res.status(400).send('Bad request')
+        return;
+    }
+    // const AMList = db.prepare('SELECT name, deadline FROM AM').all()
+    // res.send(AMList)
 }
--- src/api/v1/files/upload_file.ts
+++ src/api/v1/files/upload_file.ts
@@ -3,15 +3,19 @@ import cors from 'cors'
 import {logger} from "@/utils/logger";
 import {stringify} from "node:querystring";
 import * as path from "node:path";
+import {dbAM} from "@/utils/loadDB";
 
 export const upload_file = (req: any, res: any) => {
     try {
-        const {id, name} = req.body;
+        // logger.debug(req.body)
+        const {id, name, assignment_id} = req.body;
         const files = req.files as Express.Multer.File[];
         if (!id || !name || files.length === 0) {
             return res.status(400).send('缺少必要的字段');
         }
         logger.connection(JSON.stringify(req.body));
+        // 将学生标记为已提交
+        dbAM.prepare(`UPDATE ${assignment_id} SET is_submitted = 1 WHERE StdID = ?`).run(id);
         res.status(200).send({message: '上传成功', files: files.map(file => file.filename)});
     } catch (error) {
         console.error('上传失败', error);
